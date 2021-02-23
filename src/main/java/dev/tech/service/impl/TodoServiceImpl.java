9ce15e1cbfa30db299176f6b60a8137eac67e11c
Hieu Minh Le <hieucoder@outlook.com>
2021-02-24 00:58:19 +0700

    Updated To-dos list HTML web page.

--- src/main/java/dev/tech/service/impl/TodoServiceImpl.java
+++ src/main/java/dev/tech/service/impl/TodoServiceImpl.java
@@ -99,7 +99,7 @@ public class TodoServiceImpl implements TodoService {
 
     @Override
     public Page<TodoDto> findAllPaginated(int page, int size, String sortField, String sortDirection) {
-    	Sort sort = sortDirection.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortField).ascending() : Sort.by(sortField).descending();
+        Sort sort = sortDirection.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortField).ascending() : Sort.by(sortField).descending();
         Pageable pageable = PageRequest.of(page - 1, size, sort);
         Page<Todo> todosPaginated = this.todoRepository.findAll(pageable);
         Page<TodoDto> todoDtosPaginated = todosPaginated.map(element -> TodoMapper.toDto(element));
