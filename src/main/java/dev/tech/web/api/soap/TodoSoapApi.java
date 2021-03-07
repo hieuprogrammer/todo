8a8556e6d5be09c3bbb415290b363cb0f91f7f07
Le <MinhHieu.Le@aia.com>
2021-03-07 20:26:13 +0700

    Restuctured project for SOAP and REST APIs. Implemented Logging using Slf4j for Todo REST API. (Haven't build Uber/Fat JAR.)

--- src/main/java/dev/tech/web/api/soap/TodoSoapApi.java
+++ src/main/java/dev/tech/web/api/soap/TodoSoapApi.java
@@ -1,5 +1,9 @@
 package dev.tech.web.api.soap;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 // TODO
 public class TodoSoapApi {
+    private final Logger logger = LoggerFactory.getLogger(TodoSoapApi.class);
 }
